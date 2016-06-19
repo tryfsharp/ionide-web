dc1c5c35704602aa3abfb8b8641eb915c275265c
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-06-19 16:55:20 +0200

    Dependencies and build

---
+++ runner/fsac.cmd
@@ -0,0 +1,3 @@
+cd %~dp0
+cd "..\paket-files\github.com\ionide\FsAutoComplete\bin\release\"
+.\FsAutoComplete.Suave.exe 81
--- src/editor.fsx
+++ src/editor.fsx
@@ -5,6 +5,10 @@ open Fable.Core
 open Fable.Import
 open Fable.Import.monaco
 
+//---------------------------------------------------
+//Initialization
+//---------------------------------------------------
+
 [<Emit("_monaco = monaco")>]
 let hack : unit = failwith "JS only"
 hack
@@ -18,5 +22,39 @@ options.language <- Some "fsharp"
 let services = createEmpty<editor.IEditorOverrideServices>
 
 
+//---------------------------------------------------
+//Features providers
+//---------------------------------------------------
+
+let hoverProvider = {
+    new languages.HoverProvider
+    with 
+        member this.provideHover(model, position, token) = 
+            let h = createEmpty<languages.Hover>
+            let ctn = createEmpty<IHTMLContentElement>
+            ctn.markdown <- Some "aaa"
+
+            let range = createEmpty<IRange>
+            range.startLineNumber <- position.lineNumber
+            range.endLineNumber <- position.lineNumber
+            range.startColumn <- position.column
+            range.endColumn <- position.column + 1.
+
+
+            h.htmlContent <- ResizeArray([ctn])
+            h.range <- range
+
+            Case1 h
+
+}
+
+
+
+//---------------------------------------------------
+//Create editor
+//---------------------------------------------------
+
+monaco.languages.Globals.registerHoverProvider("fsharp", hoverProvider)
+
+
 let editor = monaco.editor.Globals.create(dom |> unbox, options, services )
- 
